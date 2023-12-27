7494d85073e2ec01ae24dd83faa7314eb1ca7f37
codedevloper13 <codedevloper13@gmail.com>
2023-12-27 23:14:18 +0530

    Added mongodb driver

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,12 +3,29 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose, { ConnectOptions } from "mongoose";
 
 const app = express();
+
+async function connectToMongoDB() {
+	try {
+		await mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string, {} as ConnectOptions);
+		console.log("Database Connected Successfully ");
+	} catch (error) {
+		console.error("Error Connecting to the Database:", error);
+	}
+}
+
+// Call the async function
+connectToMongoDB();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.get("/", async (req: Request, res: Response) => {
+	res.json({ message: "Hello World this is frontend" });
+});
 app.get("/api/test", async (req: Request, res: Response) => {
 	res.json({ message: "Hello World" });
 });
